14d07c9a8049fa8cb3b511e6b2e6df9e1b374dce
FullTimeSchooler <143832567+FullTimeSchooler@users.noreply.github.com>
2024-05-14 14:13:20 -0700

    Added some Css and JS

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.addEventListener('click', e => {
     })
 })
 
-var slideshow = 1;
+var slideIndex = 1;
 showslides(slideIndex);
 
 function plusSlides(n) {
@@ -27,8 +27,16 @@ function currentSlide(n) {
 
 function showSlides(n) {
     var i;
-    var slides = document.getElementsByName("myslides");
-    var dots = document.getElementsByName("dot");
+    var slides = document.getElementsByClassName("myslides");
+    var dots = document.getElementsByClassName("dot");
     if (n > slides.length) {slideshow = 1}
     if (n < 1) {slidesIndex = slides.length}
+    for ( i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length, i++) {
+        dots[i].className = dots[i].className.replace("active", "")
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += "active";
 }
